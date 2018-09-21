efe2155e9e085523ac92b7c6d942b3907f340f32
Nicole D <nnardella@gmail.com>
2018-09-21 16:26:06 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -15,3 +15,25 @@ end
 
 
 countdown(10)
+
+
+def countdown(number)
+
+
+number = 10
+
+while number > 0
+  puts "#{number} SECOND(S)!"
+
+sleep 1
+number -= 1
+
+end
+
+"HAPPY NEW YEAR!"
+
+end
+
+
+
+countdown(10)
